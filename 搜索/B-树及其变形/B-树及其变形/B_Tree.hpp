742333fc896c4718d17a0ecaa23a58384d025d6f
石俊涛 <1550637676@qq.com>
2017-06-05 23:26:53 +0800

    B-Tree

--- 搜索/B-树及其变形/B-树及其变形/B_Tree.hpp
+++ 搜索/B-树及其变形/B-树及其变形/B_Tree.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <iostream>
+using namespace std;
+
 #ifndef _B_TREE_
 #define _B_TREE_
 
-template <typename K， size_t M>
+template <typename K, size_t M>
 struct B_TreeNode
 {
 	typedef B_TreeNode<K, M> Node;
@@ -22,10 +25,44 @@ struct B_TreeNode
 	Node* _pSub[M+1];//孩子节点集合（指针数组），多给一个空间，方便分裂操作
 };
 
-template <typename K>
+template <typename K, size_t M>
 class B_Tree
 {
+	typedef B_TreeNode<K, M> Node;
 
+public:
+	B_Tree()
+		: _pRoot(nullptr)
+	{}
+
+	bool Insert(const K& key)
+	{
+		//B树为空时
+		if (nullptr == _pRoot)
+		{
+			_pRoot = new Node;
+			_pRoot->_key = key;
+			_pRoot->_size = 1;
+
+			return true;
+		}
+			
+		//B树不为空
+
+	}
+
+	bool Find(const K& key)
+	{
+		pair<Node*, int> address = _find(key);
+		
+		return (-1 == address.second) ? true; false;
+	}
+private:
+	//返回-1存在
+	pair<Node*, int> _find(const K& key);
+
+private:
+	Node* _pRoot;
 };
 
 
